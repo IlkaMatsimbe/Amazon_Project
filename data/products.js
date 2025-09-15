a96429806214361bce9ac8b29e964f1d13db70c9
IlkaMatsimnbe <ilkamatsimbe6@gmail.com>
2025-09-15 16:00:38 +0200

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -63,12 +63,44 @@ class Clothing extends Product {
     Size chart
     </a>
     `;
-
   }
-
 }
 
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 10,
+  b: this.a
+};
+*/
+
+
+
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+
+
+this // aponta para um objecto externo, pode ser usado em qualquer lugar do codigo
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+*/
 
 export const products = [
   {
